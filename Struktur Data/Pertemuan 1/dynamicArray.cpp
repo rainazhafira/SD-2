0725a20caa85bf1fcfac3cc4ba68ddc3259500dc
rainazhafira <rainazhafira60@gmail.com>
2026-09-09 20:34:47 +0700

    Pertemuan 1 : Array & Dynamic Array

---
+++ Struktur Data/Pertemuan 1/dynamicArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukkan jumlah elemen array : ";
+    cin >> n;
+
+    int*arr = new int[n]; // dynamic array
+
+    cout << "Masukkan " << n << "angka : \n";
+    for (int i=0; i < n; i++) {
+        cin >> arr[i];
+
+    }
+        cout << "Isi array : ";
+        for(int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        
+        }
+
+        delete[] arr;
+        return 0;
+        }
---
+++ Struktur Data/Pertemuan 1/latihan1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array [3][3][4];
+
+    int angka = 2;
+
+    // mengisi array dengan bilangan ganjil
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+
+            for (int k = 0; k < 4; k++){
+                array[i][j][k] = angka;
+                angka += 2;
+            }
+        }
+    }
+
+    // menampilkan array
+    for (int i = 0; i < 3; i++) {
+        cout << "Lapis " << i+1 << endl;
+
+        for(int j = 0; j < 3; j++) {
+            for(int k = 0; k < 4; k++){
+                cout << array[i][j][k] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
